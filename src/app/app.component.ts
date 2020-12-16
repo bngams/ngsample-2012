e16256eea2714146c76e6f437c65619641a9ea66
bngams <n.boris@devidia.net>
2020-12-16 09:45:02 +0100

    base template syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', // balise, html tag
+  templateUrl: './app.component.html', // template
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'sample-ngapp';
+  imgUrl = 'https://via.placeholder.com/150';
+  isDisabled = true;
+  logoColor = 'blue';
+  today = new Date();
+  classes = { first: this.isDisabled, second: this.isDisabled, third: !this.isDisabled };
+  fruits: string[] = ['banana', 'apple', 'cherry'];
+
+  toggleButton(): void {
+    this.isDisabled = !this.isDisabled;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+    // MyCustomPipe,
+    // MyCustomDirective
   ],
   imports: [
     BrowserModule,
